53f99122bc8d8525a4c1f1ab11dbabe0b5bc001e
auto-implement <auto-implement@local>
2026-05-10 09:23:54 +0000

    [T005] SummaryReader に縮小: SQLiteSummaryRepository 削除 / resolve_and_delete 追加 / テスト全面改修

--- src/pdfsum/repositories/__init__.py
+++ src/pdfsum/repositories/__init__.py
@@ -1,9 +1,9 @@
 """データレイヤー (Repository パターン)"""
 
 from pdfsum.repositories.base import SummaryRepository
-from pdfsum.repositories.sqlite import SQLiteSummaryRepository
+from pdfsum.repositories.sqlite import SummaryReader
 
 __all__ = [
     "SummaryRepository",
-    "SQLiteSummaryRepository",
+    "SummaryReader",
 ]
